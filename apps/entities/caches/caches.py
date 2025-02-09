48e4e51d29742227daac215fe281631ff352ae5b
Jae hyeon <wjs2063@naver.com>
2025-02-09 13:42:22 +0900

    feat : add memory_chat_model example

--- apps/entities/caches/caches.py
+++ apps/entities/caches/caches.py
@@ -1,6 +1,7 @@
 from langchain_redis import RedisCache
 from langchain.globals import set_llm_cache
+from apps.infras.redis._redis import _redis_url
 
-redis_cache = RedisCache(redis_url="redis://127.0.0.1:6378")
+redis_cache = RedisCache(redis_url=_redis_url)
 
 set_llm_cache(redis_cache)
