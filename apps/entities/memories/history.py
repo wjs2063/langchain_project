e698747a9710429a672693a8261391ae69d25cbb
Jae hyeon <wjs2063@naver.com>
2025-02-12 22:28:01 +0900

    feat : async call

--- apps/entities/memories/history.py
+++ apps/entities/memories/history.py
@@ -9,64 +9,39 @@ import asyncio
 import redis.asyncio as redis
 
 
-# class AsyncRedisChatMessageHistory(BaseChatMessageHistory):
+# class AsyncRedisChatMessageHistory(RedisChatMessageHistory):
 #
 #     def __init__(
-#             self,
-#             session_id: str,
-#             url: str = "redis://localhost:6379/0",
-#             key_prefix: str = "message_store:",
-#             buffer_size: int = 8,
-#             ttl: Optional[int] = None,
+#         self,
+#         session_id: str,
+#         redis_url: str = "redis://localhost:6379/0",
+#         key_prefix: str = "message_store:",
+#         buffer_size: int = 8,
+#         ttl: Optional[int] = None,
 #     ):
-#         self.redis_client = redis.Redis.from_url(url)
+#         self.redis_client = redis.Redis.from_url(redis_url)
 #         self.session_id = session_id
 #         self.buffer_size = buffer_size
 #         self.key_prefix = key_prefix
 #         self.ttl = ttl
-#         self.loop = asyncio.new_event_loop()
-#
-#     @property
-#     def key(self) -> str:
-#         return self.key_prefix + self.session_id
-#
-#     @property
-#     def messages(self):
-#         return asyncio.run_coroutine_threadsafe(
-#             self.aget_messages(), self.loop
-#         ).result()
-#
-#     async def aget_messages(self) -> list[BaseMessage]:  # type: ignore
-#         _items = await self.redis_client.lrange(self.key, 0, -1)
-#         items = [json.loads(m.decode("utf-8")) for m in _items[::-1]]
-#         messages = messages_from_dict(items)
-#         return messages
 #
 #     def add_message(self, message: BaseMessage) -> None:
-#         asyncio.run_coroutine_threadsafe(self.aadd_message(message), self.loop)
-#
-#     async def aadd_message(self, message: BaseMessage) -> None:
-#         await self.redis_client.lpush(self.key, json.dumps(message_to_dict(message)))
-#         await self.redis_client.ltrim(self.key, 0, self.buffer_size - 1)
+#         """Append the message to the record in Redis"""
+#         self.redis_client.lpush(self.key, json.dumps(message_to_dict(message)))
+#         self.redis_client.ltrim(self.key, 0, self.buffer_size - 1)
 #         if self.ttl:
-#             await self.redis_client.expire(self.key, self.ttl)
-#
-#     def clear(self):
-#         asyncio.run_coroutine_threadsafe(self.aclear(), self.loop)
-#
-#     async def aclear(self) -> None:
-#         await self.redis_client.delete(self.key)
+#             self.redis_client.expire(self.key, self.ttl)
 
 
 class SlidingWindowBufferRedisChatMessageHistory(RedisChatMessageHistory):
 
     def __init__(
-            self,
-            session_id: str,
-            url: str,
-            buffer_size: int = 8,
-            key_prefix: str = "message_store:",
-            ttl: Optional[int] = None,
+        self,
+        session_id: str,
+        url: str,
+        buffer_size: int = 8,
+        key_prefix: str = "message_store:",
+        ttl: Optional[int] = None,
     ):
         super().__init__(session_id, url, key_prefix=key_prefix, ttl=ttl)
         self.buffer_size = buffer_size
@@ -77,6 +52,7 @@ class SlidingWindowBufferRedisChatMessageHistory(RedisChatMessageHistory):
         self.redis_client.ltrim(self.key, 0, self.buffer_size - 1)
         if self.ttl:
             self.redis_client.expire(self.key, self.ttl)
+
 
 #
 # history = AsyncRedisChatMessageHistory(
