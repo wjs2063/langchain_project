8b294dd218386dc61dda766758cbb303e4138207
Jae hyeon <wjs2063@naver.com>
2025-03-01 16:30:56 +0900

    feat : add google calendar api (GET method)

--- apps/infras/db/milvus.py
+++ apps/infras/db/milvus.py
@@ -2,6 +2,7 @@ from pymilvus import DataType, MilvusClient
 
 client = MilvusClient(
     uri="http://127.0.0.1:19530",
+    # uri="http://172.30.1.38:19530"
 )
 
 # res = client.describe_index(collection_name="incar_example_LX3")
