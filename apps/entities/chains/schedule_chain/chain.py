1ae587e653388280a5436b269cb3d141c63652b5
Jae hyeon <wjs2063@naver.com>
2025-04-15 00:04:44 +0900

    refactor : schedule chain logic 변경

--- apps/entities/chains/schedule_chain/chain.py
+++ apps/entities/chains/schedule_chain/chain.py
@@ -1,24 +1,28 @@
-from langchain.agents import create_tool_calling_agent, AgentExecutor
 from apps.entities.chat_models.chat_models import base_chat
-from apps.entities.tools.schedules.schedule_tool import (
-    fetch_my_schedule,
-    add_my_schedule,
-)
 from apps.entities.chains.schedule_chain.prompt import schedule_prompt
+from apps.entities.chains.schedule_chain.schema import ScheduleCommandModel
+from langchain_core.output_parsers import JsonOutputParser
+from apps.entities.chains.schedule_chain.prompt import schedule_response_prompt
+from apps.infras.exeternal_apis.google.calendar.handler import GoogleCalendarHandler
+import os
+from dotenv import load_dotenv
+from abc import ABC, abstractmethod
+from aiohttp import ClientSession
 
-schedule_agent = create_tool_calling_agent(
-    llm=base_chat, tools=[fetch_my_schedule, add_my_schedule], prompt=schedule_prompt
-)
-
-schedule_agent = AgentExecutor(
-    agent=schedule_agent,
-    tools=[fetch_my_schedule, add_my_schedule],
-    verbose=True,
-    max_iterations=6,
-    max_execution_time=8,
-    handle_parsing_errors=True,
-    early_stopping_method="force",
-)
+load_dotenv()
+# schedule_agent = create_tool_calling_agent(
+#     llm=base_chat, tools=[fetch_my_schedule, add_my_schedule], prompt=schedule_prompt
+# )
+#
+# schedule_agent = AgentExecutor(
+#     agent=schedule_agent,
+#     tools=[fetch_my_schedule, add_my_schedule],
+#     verbose=True,
+#     max_iterations=6,
+#     max_execution_time=8,
+#     handle_parsing_errors=True,
+#     early_stopping_method="force",
+# )
 # import pytz
 # from datetime import datetime, timezone
 #
@@ -30,3 +34,37 @@ schedule_agent = AgentExecutor(
 #         {"question": "내일 3시 휴대폰 수리 일정 등록해줘", "user_info": current_time}
 #     )
 # )
+
+
+schedule_response_chain = schedule_response_prompt | base_chat
+
+output_parser = JsonOutputParser(pydantic_object=ScheduleCommandModel)
+
+
+prompt = schedule_prompt.partial(
+    format_instructions=output_parser.get_format_instructions()
+)
+
+schedule_command_select_chain = prompt | base_chat | output_parser
+
+
+# schedule_command_response = schedule_command_select_chain.invoke(
+#     {
+#         "chat_history": [],
+#         "question": "내일 일정 알려줘",
+#         "current_datetime": datetime.now(ZoneInfo("Asia/Seoul")).isoformat(),
+#     }
+# )
+# print(schedule_command_response)
+#
+# schedule_read_command_executor = ScheduleReadCommandExecutor(
+#     schedule_command_response=schedule_command_response
+# )
+#
+# schedule_register_command_executor = ScheduleRegisterCommandExecutor(
+#     schedule_command_response=schedule_command_response
+# )
+import asyncio
+
+# asyncio.run(schedule_read_command_executor.aexecute())
+# asyncio.run(schedule_register_command_executor.aexecute())
---
+++ apps/infras/exeternal_apis/google/calendar/handler.py
@@ -0,0 +1,98 @@
+from aiohttp import ClientSession
+import jwt
+import time
+import os
+import json
+from dotenv import load_dotenv
+import requests
+from datetime import datetime, timedelta
+import pytz
+from aiohttp import ClientSession
+
+load_dotenv()
+
+
+class GoogleCalendarHandler:
+
+    @staticmethod
+    async def fetch_calendar_events(calendar_id: str):
+        async with ClientSession() as session:
+            async with session.get(
+                f"https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events?key=<KEY>"
+            ) as response:
+                return await response.json()
+
+    @staticmethod
+    async def register_calendar_event(calendar_id: str, event: dict):
+        async with ClientSession() as session:
+            async with session.post(
+                f"https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events?key=<KEY>",
+                json=event,
+            ) as response:
+                pass
+
+    @staticmethod
+    async def delete_calendar_event(calendar_id: str, event_id: str):
+        pass
+
+    @staticmethod
+    async def fetch_google_calendar_access_token(signed_jwt: str = None):
+        """
+        Fetches an access token for Google Calendar API using a signed JWT.
+
+        This function creates a signed JWT token and exchanges it for an access token
+        from Google's OAuth 2.0 endpoint. The access token is required for making
+        authenticated requests to the Google Calendar API.
+
+        Returns:
+            str: The obtained access token for accessing Google Calendar API.
+
+        Raises:
+            KeyError: If the response JSON does not contain the "access_token" key.
+        """
+        signed_jwt = create_google_jwt_token()
+
+        google_oauth_url = "https://oauth2.googleapis.com/token"
+        data = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+            "assertion": signed_jwt,
+        }
+        async with ClientSession() as session:
+            async with session.post(url=google_oauth_url, json=data) as response:
+                response = await response.json()
+                return response["access_token"]
+
+
+def create_google_jwt_token():
+    """
+    Generate a Google JWT token for authentication
+
+    This function creates a JSON Web Token (JWT) to authenticate with Google API services.
+    The payload includes information such as the issuing entity, scope, audience, issued time,
+    and expiration time. The token is signed using RS256 algorithm.
+
+    Returns:
+        str: The signed JWT token.
+
+    Raises:
+        EnvironmentError: If one or more of the required environment variables are not set.
+    """
+    iat = time.time()
+    exp = iat + 3600
+    payload = {
+        "iss": os.getenv("MY_GOOGLE_CALENDAR_EMAIL"),
+        # "sub": os.getenv("MY_GOOGLE_CALENDAR_ID"),
+        "scope": "https://www.googleapis.com/auth/calendar",
+        # "https://www.googleapis.com/auth/calendar.readonly",
+        "aud": "https://oauth2.googleapis.com/token",
+        "iat": iat,
+        "exp": exp,
+    }
+    additional_headers = {"kid": os.getenv("GOOGLE_CALENDAR_SERVICE_KEY_ID")}
+    signed_jwt = jwt.encode(
+        payload,
+        os.getenv("GOOGLE_CALENDAR_SERVICE_KEY_PASSWORD"),
+        headers=additional_headers,
+        algorithm="RS256",
+    )
+    return signed_jwt
